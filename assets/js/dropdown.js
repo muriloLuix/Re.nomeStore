2a5a1fc3711c565834fa6b6d7913fdbf92798a1a
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-06-06 01:18:33 -0300

    Ajustando responsividade em algumas telas

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    $('.dropdown-toggle').click(function () {
-        $(this).next('.dropdown').toggleClass('open');
-    });
+  $(".dropdown-toggle").click(function (e) {
+    e.stopPropagation();
+    $(this).children(".dropdown").toggleClass("open");
+  });
 
-    $(document).click(function (event) {
-        if (!$(event.target).closest('.nav').length) {
-            // Fecha o menu se estiver aberto
-            $('.dropdown').removeClass('open');
-        }
-    });
+  $(document).click(function (event) {
+    if (!$(event.target).closest(".nav").length) {
+      // Fecha o menu se estiver aberto
+      $(".dropdown").removeClass("open");
+    }
+  });
 });
